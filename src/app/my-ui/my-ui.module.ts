5a63116168a8abf77af7aae0caea14e5376c1c02
Thang Luu <you@example.com>
2020-02-12 21:05:47 +0700

    Create Login Dialog

--- src/app/my-ui/my-ui.module.ts
+++ src/app/my-ui/my-ui.module.ts
@@ -3,14 +3,26 @@ import { CommonModule } from '@angular/common';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { LogInDialogComponent } from './log-in-dialog/log-in-dialog.component';
 import { FooterComponent } from './footer/footer.component';
+import { MatInputModule } from '@angular/material/input';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatDialogModule} from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+
+
+
 
 
 
 @NgModule({
   declarations: [NavBarComponent, LogInDialogComponent, FooterComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MatInputModule,
+    MatTabsModule,
+    MatDialogModule,
+    MatButtonModule,
+
   ],
-  exports: [NavBarComponent]
+  exports: [NavBarComponent,LogInDialogComponent]
 })
 export class MyUiModule { }
