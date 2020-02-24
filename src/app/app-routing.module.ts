39f02e2c2d076d17e29cf3d1e8fe38cfb52bb256
thangluu93 <luuthang9300@gmail.com>
2020-02-24 14:32:17 +0700

    move foleder playing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [{ path: '', loadChildren: () => import('./pages/home-page/home-page.module').then(m => m.HomePageModule) },
+const routes: Routes = [{ path: 'homePage', loadChildren: () => import('./pages/home-page/home-page.module').then(m => m.HomePageModule) },
 { path: 'game-play', loadChildren: () => import('./pages/game-play/game-play.module').then(m => m.GamePlayModule) },
-{ path: 'playing', loadChildren: () => import('./pages/game-play/playing/playing.module').then(m => m.PlayingModule) }];
+{ path: '', loadChildren: () => import('./pages/playing/playing.module').then(m => m.PlayingModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
