65504e608fa0b89349282b0f9393d876f0f3e520
Hoang Thien <hoangthien22021999@gmail.com>
2020-02-24 12:06:59 +0700

    before set components

--- src/app/pages/game-play/playing/playing.module.ts
+++ src/app/pages/game-play/playing/playing.module.ts
@@ -3,13 +3,24 @@ import { CommonModule } from '@angular/common';
 
 import { PlayingRoutingModule } from './playing-routing.module';
 import { PlayingComponent } from './playing.component';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+
 
 
 @NgModule({
   declarations: [PlayingComponent],
   imports: [
     CommonModule,
-    PlayingRoutingModule
+    PlayingRoutingModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatToolbarModule
+    
   ]
 })
 export class PlayingModule { }
