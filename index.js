9ce97b08ef1b44cde0813c6b12569b66e3bdb778
leinailychee <leinailychee__@users.noreply.github.com>
2020-02-26 11:41:48 +0700

    edit revover

--- index.js
+++ index.js
@@ -2,43 +2,26 @@ const app = require("express")();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 
-app.get("/", (req, res) => {
-  res.send("<h1>Hey Socket.io</h1>");
+// array=[wolf,seer,villager,witch,hunter,bodyguard]
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hey Socket.io</h1>');
 });
 
-recorverMessage=[];
+io.on('connection', (socket) => {
+  console.log("-------" + socket.id+ "--------");
+  socket.on('send', function (data) {
+    console.log(data)
+    // recorverMessage.push(data);
+    io.emit('reply', data);
+  })
+})
+
+
 
 http.listen(3000, () => {
-  console.log("listening on *:3000");
+  console.log('listening on *:3000');
 });
 
-user=[]
+// shuffle(arr);
 
-io.on("connection", (socket) => {
-  console.log( socket.id +"--------connected---------");
-
-
-  if (socket.id!="" && recorverMessage.length!=0 ) {
-    // recorverMessage.map(eachVaule=>{
-    //   io.emit('roomRecover',eachVaule)
-    // })
-    io.emit('roomRecover',recorverMessage)
-    console.log("Does someone new entered ?")
-    console.log("-")
-    console.log(recorverMessage)
-    console.log("-")
-    // user.push(socket.id)
-  }
- 
-  socket.on('send', function (data) {
-   
-    console.log(data)
-    recorverMessage.push(data);
-    io.emit('my broadcast',data);
-    }
-  );
-
-  socket.on("disconnect", () => {
-    console.log("user disconnected");
-  });
-});
