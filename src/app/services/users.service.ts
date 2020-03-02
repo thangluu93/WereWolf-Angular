6e94bb2a7f15b735f07c868ec2fd0fc8453ab0b0
thangluu93 <luuthang9300@gmail.com>
2020-03-02 22:14:21 +0700

    add observable to logi with GG

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,45 +1,66 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
-import {  AngularFireAuth} from '@angular/fire/auth'
-import { auth} from 'firebase';
-import {SocketioService} from './socketIo/socket-io.service'
+import { AngularFireAuth } from '@angular/fire/auth'
+import { auth } from 'firebase';
+import { SocketioService } from './socketIo/socket-io.service'
+import * as firebase from 'firebase/app'
+import { ResolveEnd } from '@angular/router';
+import { resolve } from 'dns';
+
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
-  
-  public user:User = {
-    email:'',
-    name:'',
-    photoURL:'',
+
+  public user: User = {
+    email: '',
+    name: '',
+    photoURL: '',
   }
 
-  
+
   uid: string = "";
   constructor(
-    public db:AngularFirestore,
-    public afAuth:AngularFireAuth,
-    public socketIo:SocketioService
+    public db: AngularFirestore,
+    public afAuth: AngularFireAuth,
+    public socketIo: SocketioService
 
   ) { }
 
-  async logingg(){
-    const provider = await new auth.GoogleAuthProvider();
-    await this.afAuth.auth.signInWithPopup(provider).then((u)=>{
-      console.log(u.user);
-      this.uid = u.user.uid;
-      this.user.email = u.user.email;
-      this.user.name = u.user.displayName;
-      this.user.photoURL = u.user.photoURL;
-      if(!this.checkuser(u.user.uid)){
-        this.adduser();
-      }
-    });
+  async loginWithGoogle() {
+    return new Promise<any>((resolve, reject) => {
+      let provider = new auth.GoogleAuthProvider();
+      this.afAuth.auth.signInWithPopup(provider).then((u) => {
+        console.log(u.user);
+        this.uid = u.user.uid;
+        this.user.email = u.user.email;
+        this.user.name = u.user.displayName;
+        this.user.photoURL = u.user.photoURL;
+        if (!this.checkUser(u.user.uid)) {
+          this.addUser();
+        }
+      })
+    })
   }
 
-  checkuser(uid:string):boolean{
-    this.db.collection('users').doc(uid).get().subscribe((data)=>{
-      if(data.exists){
+  doFacebookLogin() {
+    return new Promise<any>((resolve, reject) => {
+      let provider = new firebase.auth.FacebookAuthProvider();
+      this.afAuth.auth
+        .signInWithPopup(provider)
+        .then(res => {
+          resolve(res);
+        }, err => {
+          console.log(err);
+          reject(err);
+        })
+    })
+  }
+
+
+  checkUser(uid: string): boolean {
+    this.db.collection('users').doc(uid).get().subscribe((data) => {
+      if (data.exists) {
         return true;
       }
       else return false;
@@ -47,17 +68,19 @@ export class UsersService {
     return false;
   }
 
-  adduser(){
+  addUser() {
     this.db.collection('users').doc(this.uid).set(this.user);
   }
 
-  sendUserData(){
-    this.socketIo.sendData("users",this.uid);
+  sendUserData() {
+    this.socketIo.sendData("users", this.uid);
   }
+
+
 }
 
-interface User{
-  email:string,
-  name:string,
-  photoURL:string,
+interface User {
+  email: string,
+  name: string,
+  photoURL: string,
 }
